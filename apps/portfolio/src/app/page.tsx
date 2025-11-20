2addb4048b514c86e584789b977a0c8f32b3e1d0
Lance Bailey <lancebailey222@gmail.com>
2025-11-19 19:05:01 -0500

    test

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
         <h1 style={{ fontSize: "2.5rem", marginBottom: "1rem" }}>Welcome to Skyforge</h1>
         <p style={{ marginBottom: "1.5rem", opacity: 0.8 }}>
         </p>
-        <Button variant="primary" size="lg" onClick={() => alert("Hello")}>Danger</Button>
+        <Button variant="primary" size="lg" onClick={() => alert("Hello From Skyforge, Welcome to Dev.")}>Danger</Button>
       </div>
     </main>
   )
